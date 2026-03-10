20a7c59c0e5ec3a4a1a651471a4f300be61f1668
sion-neko <shionbaijin@gmail.com>
2026-03-11 00:12:17 +0900

    feat: 要約コードの追加

--- speach2text.py
+++ speach2text.py
@@ -1,7 +1,22 @@
 from faster_whisper import WhisperModel
+import requests
+
 
 model = WhisperModel("large-v3", device="cpu") 
-segments, info = model.transcribe("sample.mp3", language="ja")
+segments, info = model.transcribe("input/sample.mp3", language="ja")
 
+text = ""
 for segment in segments:
-    print(segment.text)
+    text += segment.text
+
+
+url = "http://localhost:11434/api/generate"
+data = {
+    "model": "qwen3.5:4b",
+    "prompt": "要約してください。" + text,
+    "stream": False
+}
+
+res = requests.post(url, json=data)
+print(res.json()["response"])
+    
