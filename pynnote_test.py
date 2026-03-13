c60795354da3e3c43fc28d786a183d76daffdaee
sion-neko <shionbaijin@gmail.com>
2026-03-13 23:25:29 +0900

    add: pyannoteの追加

---
+++ pynnote_test.py
@@ -0,0 +1,6 @@
+from pyannote.audio import Pipeline
+pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1", use_auth_token=True)
+diarization = pipeline("input/audio.wav")
+
+for turn, _, speaker in diarization.itertracks(yield_label=True):
+    print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
