253e682244b9c33205dfb00bd5d4b37ecd830c6f
sion-neko <shionbaijin@gmail.com>
2026-03-29 00:06:12 +0900

    feat: APIを実行できるように修正

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import shutil
 from audiotool.core import process_audio
+
 
 # GPUライブラリ (CUDA/cuDNN) のパスを動的に追加
 base_path = os.path.dirname(os.path.abspath(__file__))
@@ -18,6 +20,10 @@ for path in cuda_paths:
         os.add_dll_directory(path)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"]
+)
 
 
 @app.get("/summarize")
@@ -26,3 +32,8 @@ def summarize(file: UploadFile = File(...)):
         shutil.copyfileobj(file.file, buffer)
     result = process_audio("audio_temp.wav", True)
     return {"message": result}
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
